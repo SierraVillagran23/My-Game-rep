1a984b043750e3ca00b714ff3298bdb34b25d043
SierraVillagran23 <sierra.villagran@gmail.com>
2023-04-24 08:44:12 -0500

    Added chest and gem
    Chest and gem are added

--- treasure_adventure/Assets/Scripts/PlayerMovement.cs
+++ treasure_adventure/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private float speed = 10.0f;
+    private float turnSpeed = 20.0f;
+    private float horizontalInput;
+    private float verticalInput;
+    private Rigidbody playerRb;
+    public float jumpforce;
+    public float gravityModifier;
+    private Animator anim;
+   
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+        anim = GetComponent<Animator>();
+       
     }
 
     // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        // player movement forward and sideways
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        Rotation();
+        // player jump
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+            
+        }
     }
+    void Rotation()
+    {
+        // easier way to rotate the player
+        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X") * 4f, 0));
+    }
+
+
 }
